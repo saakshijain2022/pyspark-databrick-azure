43f30d904ebda62887909a7522664d24ed22c616
saakshijain2022 <saakshijain2022>
2023-07-25 12:49:29 +0000

    Third Commit

--- Pyspark.py
+++ Pyspark.py
@@ -13,7 +13,7 @@ display(df)
 
 dbutils.fs.mount(
 source = "wasbs://raw@saakshistorage2.blob.core.windows.net",
-mount_point = "/mnt/test_db_2.0/outputs",
+mount_point = "/mnt/test_db_2.0/output",
 extra_configs = {"fs.azure.account.key.saakshistorage2.blob.core.windows.net":"/gt6jmiEPXOIuYd+agoYPsUwOt9QrnOVFqFrK3yRIXNYdHbpVpHABuwvdKSK8JGpwutO/MDgMBPl+AStp+A5AQ=="})
 
 # COMMAND ----------
@@ -21,12 +21,84 @@ extra_configs = {"fs.azure.account.key.saakshistorage2.blob.core.windows.net":"/
 from pyspark.sql.functions import *
 import urllib
 
-df1 = spark.read.format("csv").option("sep",",").option("header","true").option("InferSchema","true").load("/mnt/test_db_2.0/outputs")
+df1 = spark.read.format("csv").option("sep",",").option("header","true").option("InferSchema","true").load("/mnt/test_db_2.0/output")
 display(df1)
 
 # COMMAND ----------
 
 df1.show(2)
+
+# COMMAND ----------
+
+# selecting specific columns
+df1.select('name','department').show()
+
+# COMMAND ----------
+
+df1.collect()
+
+# COMMAND ----------
+
+# "First two rows"
+df1.take(2)
+
+# "Last two rows"
+df1.tail(2)
+
+# COMMAND ----------
+
+# act as info in pandas
+df1.describe().show()
+
+# COMMAND ----------
+
+# group by
+
+df1.groupby("location").avg().show()
+
+# COMMAND ----------
+
+# filter
+df1.filter(df1.user == 1).show()
+
+# COMMAND ----------
+
+df1.filter(df1.name == 'mani').show()
+
+# COMMAND ----------
+
+df1.filter(df1.salary >=10000).show()
+
+# COMMAND ----------
+
+# rename
+df2 = df1.withColumnRenamed('location','loc')
+
+# COMMAND ----------
+
+df2.show()
+
+# COMMAND ----------
+
+# sort
+df1.sort(df1.salary.desc()).show()
+
+# COMMAND ----------
+
+df1.orderBy(df1.salary.desc()).show()
+
+# COMMAND ----------
+
+# metrics -- find out total salary based on location 
+df1.groupby('location').sum('salary').show()
+
+# COMMAND ----------
+
+df1.groupby('location','date').sum('salary').show()
+
+# COMMAND ----------
+
+df1.groupby('location','attendance').sum('salary').show()
 
 # COMMAND ----------
 
